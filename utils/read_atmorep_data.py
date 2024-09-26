a55e8eeda49ecce4978a0c2f33aec7d31da736db
Simon Grasse <s.grasse@fz-juelich.de>
2024-09-26 07:33:59 +0200

    add: debug print statements

--- utils/read_atmorep_data.py
+++ utils/read_atmorep_data.py
@@ -300,12 +300,16 @@ class HandleAtmoRepDataDask(HandleAtmoRepData):
         return [da]
 
     def _read_data_parallel(self, datapath: pl.Path, varname: str, compute: bool):
+        print("setup reader")
         samples = Samples(datapath, varname)
         chunky = ChunkedData(samples)
 
+        print("start iniatializing dask array")
         dask_array = da.empty(shape=chunky.shape, chunks=chunky.chunks)
         data = xr.DataArray(dask_array, coords=chunky.global_coords, dims=chunky.dims)
+        print("map data loading function")
         loaded_data = data.map_blocks(chunky.load_chunk, template=data)
+        print("finished loading data")
         if compute:
             return loaded_data.compute()
 
